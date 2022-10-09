866ca9ce0849afd2766a4ff73cbbd32377fb98c0
hery <bienaimehery1@gmail.com>
2022-10-09 19:03:26 +0300

    modification page message ajout text field en bas de la page version beta

--- lib/interface/message.dart
+++ lib/interface/message.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 class Message extends StatefulWidget {
-  const Message({ Key? key }) : super(key: key);
+  const Message({Key? key}) : super(key: key);
 
   @override
   State<Message> createState() => _ProjectState();
@@ -13,8 +14,39 @@ class _ProjectState extends State<Message> {
       appBar: AppBar(
         backgroundColor: Colors.red,
         title: Text('Messages'),
-      )
-      
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  RaisedButton(onPressed: null),
+                  RaisedButton(onPressed: null)
+                ],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 600,
+                ),
+                _textfield()
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
+
+  _textfield() => TextFormField(
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'tapez ici',
+          labelText: 'votre message',
+        ),
+      );
 }
